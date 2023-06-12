4fba8c1d887e4c653e39d16b2d421f1927b4ac5e
Luciano-Lima <ccll4ever1@gmail.com>
2023-06-12 17:59:35 +0100

    Creating the path for the update and delete views

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.cart, name='cart'),
-    path('cart/add/<int:product_id>', views.add_to_cart, name='add_to_cart')
+    path('cart/add/<int:product_id>', views.add_to_cart, name='add_to_cart'),
+    path('cart/update/<int:product_id>', views.update_cart, name='update_cart'),
+    path('cart/delete/<int:product_id>', views.delete_cart, name='delete_cart')
 ]
