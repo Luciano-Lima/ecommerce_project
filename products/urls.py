16bc77ec75b7941cf4a67c3e548a1f093526074f
Luciano-Lima <ccll4ever1@gmail.com>
2023-06-12 01:52:45 +0100

    Creating path for categories

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<slug:category_slug>', views.home, name='products_by_category'),
-    path('<slug:category_slug>', views.productDetail, name='products_detail'),
+    # path('products/', views.home, name='',
+    path('<slug:category_slug>', views.categories, name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>', views.productDetail, name='product_detail'),
 ]
