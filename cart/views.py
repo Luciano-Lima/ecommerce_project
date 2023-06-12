ccca94db3115911b744fb9e26c0421ed7de58873
Luciano-Lima <ccll4ever1@gmail.com>
2023-06-12 17:11:07 +0100

    Created Add_to_Cart views

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from products.models import Products
+from django.contrib import messages
 
 # Create your views here.
+# Render the home page for cart
 def cart(request):
     return render(request, 'cart.html')
+
+#Add item to cart 
+def add_to_cart(request, product_id):
+    quantity = int(request.POST.get('quantity') or 0)#prevents literal for int() error
+    if quantity == 0:
+        messages.warning(request,'Please enter a quantity!')
+        
+    cart = request.session.get('cart', {})
+    if product_id in cart:
+        cart[product_id] = int(cart[product_id]) + quantity      
+    else:
+        cart[product_id] = cart.get(product_id, quantity) 
+    request.session['cart'] = cart
+    return redirect(reverse('home'))
