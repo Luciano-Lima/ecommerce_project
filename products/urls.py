30da5e9d9b8a895f2dd031db595bff880288e2ff
Luciano-Lima <ccll4ever1@gmail.com>
2023-06-12 11:06:23 +0100

    Removed unused path from /products

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('products/', views.home, name='',
     path('<slug:category_slug>', views.categories, name='products_by_category'),
     path('<slug:category_slug>/<slug:product_slug>', views.productDetail, name='product_detail'),
 ]
