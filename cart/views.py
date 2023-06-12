057559d296e4c7f1c6b53c0f4b6ad9f54e947924
Luciano-Lima <ccll4ever1@gmail.com>
2023-06-12 17:58:20 +0100

    Creating the view for update and delete cart

--- cart/views.py
+++ cart/views.py
@@ -20,3 +20,31 @@ def add_to_cart(request, product_id):
         cart[product_id] = cart.get(product_id, quantity) 
     request.session['cart'] = cart
     return redirect(reverse('home'))
+
+
+def update_cart(request, product_id):
+    """Add or Remove itens from cart"""
+    quantity = int(request.POST.get('quantity')or 0)
+    if quantity == 0:  
+        messages.warning(request,'Enter a new Quantity or Proceed to Checkout!')
+    else:
+        cart = request.session.get('cart',{})
+        products = Products.objects.all()
+        if quantity >= 0:
+            cart[product_id] = quantity
+            messages.success(request,'Your cart was updated sucessfully!')
+        else:
+            cart.pop(product_id)
+        request.session['cart'] = cart
+    return redirect(reverse('cart'))
+
+
+def delete_cart(request, product_id):
+    quantity = int(request.POST.get('quantity')or 0)
+    cart = request.session.get('cart',{})
+    products = Products.objects.all()
+    if quantity >= 0:
+        cart.pop(product_id)
+        request.session['cart'] = cart
+        messages.success(request,'Item removed from your Cart!')
+    return redirect(reverse('cart'))
