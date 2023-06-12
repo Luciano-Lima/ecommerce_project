1da8995b82069d447fde08d79d9ef73b3b188001
Luciano-Lima <ccll4ever1@gmail.com>
2023-06-12 19:04:56 +0100

    removed path name products form the begining of path

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
-    path('cart/', include('cart.urls')),
+    path('', include('cart.urls')),
     path('', categories, name='categories'),
 ]
 
