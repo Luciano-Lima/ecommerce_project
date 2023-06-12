b5060255f7d6797f358f491fe3082011a5c71de1
Luciano-Lima <ccll4ever1@gmail.com>
2023-06-12 20:49:06 +0100

    Added search app to installed_apps

---
+++ search/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from products.models import Products
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+
+def search_products(request):
+    products_items = Products.objects.all().order_by('name')
+    query = request.GET.get('q')
+    if query:
+        products_items = Products.objects.filter(name__icontains=query).order_by('name')
+    paginator = Paginator(products_items, 2)
+    page = request.GET.get('page')
+    try: 
+        product = paginator.page(page)
+    except PageNotAnInteger:
+        product = paginator.page(1) #returns the first page if page not integer
+    except EmptyPage:
+        product = paginator.page(paginator.num_pages) #returns last page if page out of range
+    
+    return render(request, "home.html", {"products": product})
