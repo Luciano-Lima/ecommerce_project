8cbf8fc59d0f69c1494b60795e50036f6b224ce3
Luciano-Lima <ccll4ever1@gmail.com>
2023-06-19 08:19:07 +0100

    Updating the cart path with "cart/"

--- cart/urls.py
+++ cart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cart, name='cart'),
+    path('cart/', views.cart, name='cart'),
     path('cart/add/<int:product_id>', views.add_to_cart, name='add_to_cart'),
     path('cart/update/<int:product_id>', views.update_cart, name='update_cart'),
     path('cart/delete/<int:product_id>', views.delete_cart, name='delete_cart')
