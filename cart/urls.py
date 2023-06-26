dbd9303c959db90b85be137fd4e3f4edfe842f93
Luciano-Lima <ccll4ever1@gmail.com>
2023-06-26 21:23:13 +0100

    Updating the delete view path from int to str

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('cart/', views.cart, name='cart'),
     path('cart/add/<int:id>', views.add_to_cart, name='add_to_cart'),
     path('cart/update/<int:id>', views.update_cart, name='update_cart'),
-    path('cart/delete/<int:id>', views.delete_cart, name='delete_cart')
+    path('cart/delete/<str:id>', views.delete_cart, name='delete_cart')
 ]
