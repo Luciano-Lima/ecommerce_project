d3ee60a9235f7bc1a41244c58bf8c6c19ec1fbb2
Luciano-Lima <ccll4ever1@gmail.com>
2023-08-05 19:50:09 +0100

    Setting up database connection

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -103,25 +103,27 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Supabase Database settings -  https://app.supabase.com
+# DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'), 
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'), 
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
-# DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ.get('DB_NAME'), 
-#         'USER': os.environ.get('DB_USER'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST'), 
-#         'PORT': os.environ.get('DB_PORT'),
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
