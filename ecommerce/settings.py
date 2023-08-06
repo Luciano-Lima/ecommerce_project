d12a42057d0f9430fa953c7801542fb6635e6837
Luciano-Lima <ccll4ever1@gmail.com>
2023-08-06 21:14:05 +0100

    Adding whitenoise files storages

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -178,6 +178,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
