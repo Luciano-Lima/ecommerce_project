63b183823480b40392e5b3c57058fa6804bdc646
Luciano-Lima <ccll4ever1@gmail.com>
2023-08-07 19:30:08 +0100

    Correcting a missing comma on whitenoise middleware

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -67,14 +67,13 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
